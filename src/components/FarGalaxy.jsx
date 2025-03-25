8350eb2401f4cc8ad383f6013071e6b3994308fb
Viktoriya-Frank <playviktory@gmail.com>
2025-03-25 21:37:08 +0100

    Modified files: constants.js FarGalaxy.jsx StarWars.jsx

--- src/components/FarGalaxy.jsx
+++ src/components/FarGalaxy.jsx
@@ -1,9 +1,25 @@
-import {openingCrawl} from "../utils/constants.js";
+import {useEffect, useState} from "react";
+import {baseUrl} from "../utils/constants.js";
 
 const FarGalaxy = () => {
+    const [openingCrawl, setOpeningCrawl] = useState('Loading...');
+
+    useEffect(() => {
+        const episode = Math.floor(Math.random() * 6 + 1);
+        fetch(`${baseUrl}/v1/films/${episode}`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`Failed to fetch films`);
+                }
+                return res.json();
+            })
+            .then(data => setOpeningCrawl(data.opening_crawl))
+            .catch(e => setOpeningCrawl(e.message));
+    }, [])
+
     return (
         <p className="farGalaxy">
-                {openingCrawl}
+            {openingCrawl}
         </p>
     );
 };
